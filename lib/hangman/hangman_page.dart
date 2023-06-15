4a17b2162815e0c6e44698dbc01620984978921d
Mohammad Jaafar <m.jaafar@innopolis.university>
2023-06-15 18:56:53 +0300

    implemented some logic in hangman so the user now knows when he passes the level or loses it with a button that allow him to play again or go to the next level

--- lib/hangman/hangman_page.dart
+++ lib/hangman/hangman_page.dart
@@ -15,6 +15,28 @@ class _HangmanPageState extends State<HangmanPage> {
   String word = 'Flutter'.toUpperCase();
   final List<String> alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').toList();
 
+  bool isGameOver() {
+    return Game.gameTries >= 6 || isWordGuessed();
+  }
+
+  void resetGame() {
+    setState(() {
+      word = 'Flutter'
+          .toUpperCase(); // replace this line with your word generating logic if you want different words each time
+      Game.selectedChar = [];
+      Game.gameTries = 0;
+    });
+  }
+
+  bool isWordGuessed() {
+    for (var letter in word.split('')) {
+      if (!Game.selectedChar.contains(letter)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,100 +54,143 @@ class _HangmanPageState extends State<HangmanPage> {
         elevation: 0,
         backgroundColor: AppColors.primaryColor,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              "assets/images/my_background_image.png",
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/my_background_image.png",
+                ),
+                fit: BoxFit.fill,
+              ),
             ),
-            fit: BoxFit.fill,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Stack(
+                    children: [
+                      figureImage(
+                          Game.gameTries >= 0, "assets/images/hang_black.png"),
+                      figureImage(
+                          Game.gameTries >= 1, "assets/images/head_black.png"),
+                      figureImage(
+                          Game.gameTries >= 2, "assets/images/body_black.png"),
+                      figureImage(
+                          Game.gameTries >= 3, "assets/images/ra_black.png"),
+                      figureImage(
+                          Game.gameTries >= 4, "assets/images/la_black.png"),
+                      figureImage(
+                          Game.gameTries >= 5, "assets/images/rl_black.png"),
+                      figureImage(
+                          Game.gameTries >= 6, "assets/images/ll_black.png"),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    spacing: 3.0,
+                    runSpacing: 3.0,
+                    children: word
+                        .split('')
+                        .map((e) => letter(e.toUpperCase(),
+                            !Game.selectedChar.contains(e.toUpperCase())))
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 250,
+                  child: GridView.count(
+                    crossAxisCount: 7,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                    padding: const EdgeInsets.all(8.0),
+                    children: alphabet.map((e) {
+                      return RawMaterialButton(
+                          onPressed:
+                              isGameOver() || Game.selectedChar.contains(e)
+                                  ? null
+                                  : () {
+                                      setState(() {
+                                        Game.selectedChar.add(e);
+                                        if (!word
+                                            .split('')
+                                            .contains(e.toUpperCase())) {
+                                          Game.gameTries++;
+                                        }
+                                      });
+                                    },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                          fillColor: Game.selectedChar.contains(e)
+                              ? word.split('').contains(e.toUpperCase())
+                                  ? Colors.green
+                                  : Colors.black
+                              : Colors.blue,
+                          child: Text(
+                            e,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontFamily: 'KristenITC',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ));
+                    }).toList(),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Stack(
-                children: [
-                  figureImage(
-                      Game.gameTries >= 0, "assets/images/hang_black.png"),
-                  figureImage(
-                      Game.gameTries >= 1, "assets/images/head_black.png"),
-                  figureImage(
-                      Game.gameTries >= 2, "assets/images/body_black.png"),
-                  figureImage(
-                      Game.gameTries >= 3, "assets/images/ra_black.png"),
-                  figureImage(
-                      Game.gameTries >= 4, "assets/images/la_black.png"),
-                  figureImage(
-                      Game.gameTries >= 5, "assets/images/rl_black.png"),
-                  figureImage(
-                      Game.gameTries >= 6, "assets/images/ll_black.png"),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                spacing: 3.0,
-                runSpacing: 3.0,
-                children: word
-                    .split('')
-                    .map((e) => letter(e.toUpperCase(),
-                        !Game.selectedChar.contains(e.toUpperCase())))
-                    .toList(),
-              ),
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-            SizedBox(
+          if (isGameOver())
+            Container(
               width: double.infinity,
-              height: 250,
-              child: GridView.count(
-                crossAxisCount: 7,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-                padding: const EdgeInsets.all(8.0),
-                children: alphabet.map((e) {
-                  return RawMaterialButton(
-                      onPressed: Game.selectedChar.contains(e)
-                          ? null
-                          : () {
-                              setState(() {
-                                Game.selectedChar.add(e);
-                                if (!word.split('').contains(e.toUpperCase())) {
-                                  Game.gameTries++;
-                                }
-                              });
-                            },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4.0),
+              height: double.infinity,
+              color: Colors.black.withOpacity(0.8),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      isWordGuessed() ? 'You Passed!' : 'You Lost!',
+                      style: const TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
-                      fillColor: Game.selectedChar.contains(e)
-                          ? word.split('').contains(e.toUpperCase())
-                              ? Colors.green
-                              : Colors.black
-                          : Colors.blue,
+                    ),
+                    SizedBox(height: 30),
+                    TextButton(
+                      onPressed: resetGame,
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
                       child: Text(
-                        e,
+                        isWordGuessed() ? 'Next Level' : 'Play Again',
                         style: const TextStyle(
+                          fontSize: 24,
                           color: Colors.white,
-                          fontSize: 30,
-                          fontFamily: 'KristenITC',
-                          fontWeight: FontWeight.bold,
                         ),
-                      ));
-                }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+
+        ],
       ),
     );
   }

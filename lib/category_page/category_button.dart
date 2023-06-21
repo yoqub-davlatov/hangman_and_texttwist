f36d8f6b3e92aa482ccdb28872882f991f9b5df6
MishaBlin <mikhail_kalinin_a@mail.ru>
2023-06-22 02:08:40 +0300

    Integrated openai API to TextTwist. Words and letters are generated before loading the TextTwist screen.

--- lib/category_page/category_button.dart
+++ lib/category_page/category_button.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import '../constants/constants.dart';
+import 'category_settings.dart';
 
-OutlinedButton categoryButton(String name) {
+OutlinedButton categoryButton(String name, bool selected, Function callback) {
   return OutlinedButton(
-    onPressed: () {},
+    onPressed: () {
+      Settings.inputCategoryController.clear();
+      Settings.selected = name;
+      callback();
+    },
     style: OutlinedButton.styleFrom(
       fixedSize: const Size(251, 44),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30),
       ),
-      side: const BorderSide(
-        color: Color(0xff3E87FF),
+      side: BorderSide(
+        color: selected ? Colors.yellow : const Color(0xff3E87FF),
         width: 3,
       ),
     ),
--- lib/text_twist/assets.dart
+++ lib/text_twist/assets.dart
@@ -1,8 +1,8 @@
 class WordsInfo {
-  static List letters = "CATPEN".split("");
+  static List letters = [];
 
   static int index = 0;
   static List typedLetters = letters.map((e) => "").toList();
 
-  static List words = ["pen", "cat", "pet"];
+  static List words = [];
 }

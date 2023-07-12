1e80b0ab6be7e1d28193d95d279f4e70066ed1a5
danii <daniilzimin4@gmail.com>
2023-07-12 19:44:10 +0300

    homepage depends on screen and new screen_size file to get size of a phone

---
+++ lib/screen_size.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+var pixelRatio = window.devicePixelRatio;
+
+//Size in physical pixels
+var physicalScreenSize = window.physicalSize;
+var physicalWidth = physicalScreenSize.width;
+var physicalHeight = physicalScreenSize.height;
+
+//Size in logical pixels
+var logicalScreenSize = window.physicalSize / pixelRatio;
+var logicalWidth = logicalScreenSize.width;
+var logicalHeight = logicalScreenSize.height.toInt();
